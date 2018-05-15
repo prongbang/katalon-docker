ba9b05e2c293238a6608a26be3a5f6c9492ff67a
prongbang <prongbang@gmail.com>
2018-05-15 14:34:06 +0700

    initial

---
+++ run_chrome.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+
+rm -rfv ./reports/chrome
+mkdir -p ./reports/chrome
+
+katalon_opts='-browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
+docker run --rm -v $(pwd)/katalon:/katalon/katalon/source:ro -v $(pwd)/reports/chrome:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
---
+++ run_firefox.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+
+rm -rfv ./reports/firefox
+mkdir -p ./reports/firefox
+
+katalon_opts='-browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
+docker run --rm -v $(pwd)/katalon:/katalon/katalon/source:ro -v $(pwd)/reports/firefox:/katalon/katalon/report -e KATALON_OPTS="$katalon_opts" katalonstudio/katalon
